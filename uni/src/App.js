6ff38ef9b7ffa69e09e4d8ce956efc0b192f8c73
wilsonhsu0102 <wilsonhsu0102@gmail.com>
2019-10-24 10:45:25 -0400

    Added route matching for different ids

--- uni/src/App.js
+++ uni/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Home from './routes/homepage';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import Home from './routes/HomePage';
+import Profile from './routes/ProfilePage';
+import Admin from './routes/AdminPage';
+import Event from './routes/EventPage';
+import { Route, Switch, BrowserRouter, useRouteMatch, useParams } from 'react-router-dom';
 /*
 import { constants } from './lib/constants';
  
@@ -14,17 +17,49 @@ connection.once('open', () => {
     console.log('MongoDB database connection established successfully!');
 });*/
 
-function App() {
-  return (
-    <div>
-        <BrowserRouter>
-          <Switch> { /* Similar to a switch statement - shows the component depending on the URL path */ }
-            { /* Each Route below shows a different component depending on the exact path in the URL  */ }
-            <Route exact path='/' component={Home}/>
-          </Switch>
-        </BrowserRouter>
-      </div>
-  );
+class App extends React.Component{
+  render() {
+    return (
+      <div>
+          <BrowserRouter>
+            <Switch> { /* Similar to a switch statement - shows the component depending on the URL path */ }
+              { /* Each Route below shows a different component depending on the exact path in the URL  */ }
+              <Route exact path='/' component={Home}/>
+            </Switch>
+            <Switch> { /* Similar to a switch statement - shows the component depending on the URL path */ }
+              { /* Each Route below shows a different component depending on the exact path in the URL  */ }
+              <Route exact path='/profile/:profileId' children={<CallWantedProfile/>}/>
+            </Switch>
+            <Switch> { /* Similar to a switch statement - shows the component depending on the URL path */ }
+              { /* Each Route below shows a different component depending on the exact path in the URL  */ }
+              <Route exact path='/admin/:adminId' children={<CallWantedAdmin/>}/>
+            </Switch>
+            <Switch> { /* Similar to a switch statement - shows the component depending on the URL path */ }
+              { /* Each Route below shows a different component depending on the exact path in the URL  */ }
+              <Route exact path="/event/:eventId" children={<CallWantedEvent/>}/>
+            </Switch>
+          </BrowserRouter>
+        </div>
+    );
+  }
+}
+
+// Calls the profile page with profileId
+function CallWantedProfile() {
+  let { profileId } = useParams();
+  return <Profile id={profileId}/>;
+}
+
+// Calls the admin page with adminId
+function CallWantedAdmin() {
+  let { adminId } = useParams();
+  return <Admin id={adminId}/>;
+}
+
+// Calls the event page with eventId
+function CallWantedEvent() {
+  let { eventId } = useParams();
+  return <Event id={eventId}/>;
 }
 
 export default App;
