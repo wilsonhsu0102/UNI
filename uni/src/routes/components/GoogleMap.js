8bb2a2ad04eba7462cd47bb4358d76ce710b0dd1
wilsonhsu0102 <wilsonhsu0102@gmail.com>
2019-10-25 18:06:42 -0400

    added css for marker

--- uni/src/routes/components/GoogleMap.js
+++ uni/src/routes/components/GoogleMap.js
@@ -13,13 +13,34 @@ class GoogleMap extends React.Component {
           };
     }
 
+    
+
     render() {
+        const Marker = (props) => {
+            const { name, id } = props;
+            return (
+              <div className="marker"
+                title={name}
+              />
+            );
+          };
         return (
             <div style={{ height: '100%', width: '100%' }}>
                 <GoogleMapReact
                     bootstrapURLKeys={{ key:'AIzaSyD1ixD08kOHRT5tKUM7Q3dem0TKU908fzA'}} // Dont do bad things with this please
                     defaultCenter={this.defaultMap.center}
                     defaultZoom={this.defaultMap.zoom}>
+                        <Marker
+                            lat={43.6629}
+                            lng={-79.3957}
+                            name="UC120"
+                        />
+                        {/* <Marker
+                            lat={43.665}
+                            lng={-79.39}
+                            name="My Marker"
+                            color="blue"
+                        /> */}
                 </GoogleMapReact>
             </div>
         );
