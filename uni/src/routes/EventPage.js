9dac657a1d2967e038d5d44cbd53f2211c5e34cb
wilsonhsu0102 <wilsonhsu0102@gmail.com>
2019-11-01 13:26:42 -0400

    Changed layout and turned list of attendees into table of attendees

--- uni/src/routes/EventPage.js
+++ uni/src/routes/EventPage.js
@@ -3,12 +3,12 @@ import './EventPage.css'
 import { Link } from 'react-router-dom';
 import HostProfile from './components/HostProfile';
 import EventPhoto from './components/EventPhoto';
-import GoogleMap from './components/GoogleMap';
+import GoogleMapMock from './components/GoogleMap';
 
 const event2 = {"eventName": "FREE! BBT!", "hostId": "2", "eventCoverPhoto": "N/A"
-, "eventLocation": "SS", "Attendees": [{"name": "Jay Chou"}, {"name": "Fred Vanvleet"}, {"name": "Pascal Siakam"}, {"name": "Higher Brothers"}, {"name": "BTS"}, {"name": "Twice"}, {"name": "Black Pink"}, {"name": "Shawn Mendes"}, {"name": "Wilson Hsu"}], 
-"eventDescription": "THE MIX & MINGLE PARTY IS BACK AGAIN THIS YEAR!! Already feeling stressed out about school, missing your family and friends from home? Still single and want to meet someone special? Or just wants to have fun but don’t know where to go? DON’T WORRY ROCSAUT got you covered!"
- + "Next Friday come party with us! With beer pong and other games available! We will be serving ALL YOU CAN DRINK cocktails to test you limit!And maybe you might just be able to meet that special one on Friday night~Don’t miss out on the fun and come join us at the party!!"}
+, "eventLocation": "SS", "Attendees": [{'name': 'Johnny Depp'}, {'name': 'Arnold Schwarzenegger'}, {'name': 'Jim Carrey'}, {'name': 'Emma Watson'}, {'name': 'Daniel Radcliffe'}, {'name': 'Leonardo DiCaprio'}, {'name': 'Tom Cruise'}, {'name': 'Brad Pitt'}, {'name': 'Morgan Freeman'}, {'name': 'Tom Hanks'}], 
+"eventDescription": "SAMPLE POST: THE FREE BBT PARTY IS BACK AGAIN THIS YEAR!! Already feeling stressed out about school, missing your family and friends from home? Still single and want to meet someone special? Or just wants to have fun but don’t know where to go? DON’T WORRY We got you covered!"
+ + "Next Friday come party with us! With bbt pong and other games available! We will be serving ALL YOU CAN DRINK bbt to test you limit! And maybe you might just be able to meet that special one on Friday night~ Don’t miss out on the fun and come join us at the party!!"}
 
 class Event extends React.Component {
     constructor(props) {
@@ -20,10 +20,20 @@ class Event extends React.Component {
     }
 
     render()  {
-        const items = [];
-        for (const [index, value] of this.attendees.entries()) {
-            items.push(<li className="item" key={index}> <button className="people"> {value.name} </button> </li>)
+        // const items = [];
+        // for (const [index, value] of this.attendees.entries()) {
+        //     items.push(<td className="item" key={index}> <button className="people"> {value.name} </button> </td>)
+        // }
+        const rows = [];
+        let length = this.attendees.length;
+        for (let i = 0; i < Math.ceil(length / 3); i++) {
+            let items = [];
+            for (let j = 0; (i * 3 + j < length) && j < 3; j++) {
+                items.push(<td className="items" key={i * 3 + j}><button className="people">{this.attendees[i * 3 + j].name}</button></td>)
+            }
+            rows.push(<tr key={i}>{items}</tr>)
         }
+        console.log(rows)
         return (
             <div className="eventPage"> 
                 <div className="container"> 
@@ -47,15 +57,16 @@ class Event extends React.Component {
                     <div className="sideBlock">
                         <h3> Event Location: </h3>
                         <div className="googleMap">
-                            <GoogleMap/>
+                            <GoogleMapMock/>
+                            
                         </div>
+                        <h3> Attendees: </h3>
                         <div className="attendees">
-                            <ul className="buttons">
-                                <li> 
-                                    Attendees:
-                                </li>
-                                {items}
-                            </ul>
+                            <table className="table">
+                                <tbody> 
+                                    {rows}
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
