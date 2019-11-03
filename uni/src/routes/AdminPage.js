2dbb21d608293f4b18253154fc4d568c0c0c057b
Victor Huang <victorhuang1201@hotmail.com>
2019-11-03 05:17:19 -0500

    Finished Admin page

--- uni/src/routes/AdminPage.js
+++ uni/src/routes/AdminPage.js
@@ -2,35 +2,73 @@ import React from 'react';
 import './components/AdminPage.css'
 import { Link } from 'react-router-dom';
 
-const users = [{"name": "Wilson Hsu"}, {"name": "Twice"}, {"name": "Higher Brothers"}, {"name": "BTS"}, {"name": "Rich Brian"}, {"name": "Black Pink"}, {"name": "Shawn Mendes"}, {"name": "Jay Chou"}, {"name": "Victor Huang"}, {"name": "Got7"}, {"name": "Day6"}, {"name": "Itzy"} ]
-
+const mockUsers = [{'name': 'Wilson Hsu'}, {'name': 'Twice'}, {'name': 'Higher Brothers'}, {'name': 'BTS'},
+				   {'name': 'Rich Brian'}, {'name': 'Blackpink'}, {'name': 'Shawn Mendes'}, {'name': 'Jay Chou'},
+				   {'name': 'Victor Huang'}, {'name': 'Got7'}, {'name': 'Day6'}, {'name': 'Itzy'} ]
+const mockEvents = [{'eventName': 'Free BBT', 'hostName': 'Wilson Hsu' }, {'eventName': 'Once Fanmeet', 'hostName': 'Twice' }, 
+					{'eventName': 'IGOT7 Fanmeet', 'hostName': 'Got7' }, {'eventName': 'Free Pants', 'hostName': 'Rich Brian' }]
+					
 class Admin extends React.Component {
     constructor(props) {
         super(props);
         this.id = this.props.id;
-		this.users = users;
+		this.users = mockUsers;
+		this.startEvents = mockEvents;
 		//Fills in mock data
 		let emptyUserList = [];
 		let id = 0;
-		for (const [index, value] of users.entries()) {
-			emptyUserList.push(<tr id={"User:" + value.name + "ID:" + id} key={"User:" + value.name + "ID:" + id}>
+		for (const [index, value] of this.users.entries()) {
+			emptyUserList.push(<tr id={'User:' + value.name + 'ID:' + id} key={'User:' + value.name + 'ID:' + id}>
 								<td class = 'TableContents'>{value.name}</td>
 								<td class = 'TableContents'>{id++}</td>
 								<td class = 'TableButtonCell'><button class = 'TableButton' onClick = {this.goToProfile}>To Profile</button></td>
 								<td class = 'TableButtonCell'><button class = 'TableButton' onClick = {this.removeUser}>X</button></td></tr>);	
 		}
+		let emptyEventList = [];
+		let cEID = 0;
+		for (let i = 0; i < this.startEvents.length; i++) {
+			emptyEventList.push(<tr id={'Event:' + this.startEvents[i].eventName + 'EventID:' + cEID} key={'Event:' + this.startEvents[i].eventName + 'EventID:' + cEID}>
+								<td class = 'TableContents'>{this.startEvents[i].eventName}</td>
+								<td class = 'TableContents'>{cEID++}</td>
+								<td class = 'TableContents'>{this.startEvents[i].hostName}</td>
+								<td class = 'TableButtonCell'><button class = 'TableButton' onClick = {this.goToEvent}>To Event</button></td>
+								<td class = 'TableButtonCell'><button class = 'TableButton' onClick = {this.goToProfile}>To Profile</button></td>
+								<td class = 'TableButtonCell'><button class = 'TableButton' onClick = {this.removeEvent}>X</button></td></tr>);
+		}
 		this.state = {
 			adminLoggedIn: true,
 			userList: emptyUserList,
-			userId: id
+			userId: id,
+			numUsers: id,
+			eventId: cEID,
+			numEvents: cEID,
+			eventList: emptyEventList,
+			statsTable: []
 		}
-        console.log("Admin ID: " + this.id);
+		let beginStats = [];
+		this.state.statsTable.push(<tr id={'AdminStatisticTNU'} key={'AdminStatisticTNU'}>
+						<td class = 'TableContents'> {'Total Number of Users'}</td>
+						<td class = 'TableContents'>{ this.state.numUsers }</td></tr>);
+		this.state.statsTable.push(<tr id={'AdminStatisticTNE'} key={'AdminStatisticTNE'}>
+						<td class = 'TableContents'> {'Total Number of Events'}</td>
+						<td class = 'TableContents'>{ this.state.numEvents }</td></tr>);
+		
+        console.log('Admin ID: ' + this.id);
     }
-	
+
 	goToProfile(e) {
 		let rowId = e.target.parentElement.parentElement.id;
-		if(rowId === "User:Wilson HsuID:0"){
+		//If statement is for mock data, will be changed later
+		if(rowId === 'User:Wilson HsuID:0' || rowId === 'Event:Free BBTEventID:0'){
 			window.location.href='http://localhost:3000/profile/0';
+		}
+    }
+	
+	goToEvent(e) {
+		let rowId = e.target.parentElement.parentElement.id;
+		//If statement is for mock data, will be changed later
+		if(rowId === 'Event:Free BBTEventID:0'){
+			window.location.href='http://localhost:3000/event/0';
 		}
     }
 	
@@ -38,22 +76,27 @@ class Admin extends React.Component {
 		e.preventDefault();
 		let currUsers = [...this.state.userList];
 		let currId = this.state.userId;
+		let currNumUsers = this.state.numUsers;
+		let currStatsTable = [...this.state.statsTable];
 		const userFullName = document.querySelector('#newUser').value;
 	    if(this.state.adminLoggedIn == true){
             console.log('adding user');
-			currUsers.push(<tr id={"User:" + userFullName + "ID:" + currId} key={"User:" + userFullName + "ID:" + currId}>
-							<td>{userFullName}</td>
-							<td>{currId}</td>
+			currUsers.push(<tr id={'User:' + userFullName + 'ID:' + currId} key={'User:' + userFullName + 'ID:' + currId}>
+							<td class = 'TableContents'>{userFullName}</td>
+							<td class = 'TableContents'>{currId}</td>
 							<td class = 'TableButtonCell'><button class = 'TableButton' onClick = {this.goToProfile}>To Profile</button></td>
 							<td class = 'TableButtonCell'><button class = 'TableButton' onClick = {this.removeUser}>X</button></td></tr>);	
 			currId++;
+			currNumUsers++;
+			currStatsTable = this.updateTable([...this.state.statsTable], 'AdminStatisticTNU', 'Total Number of Users', currNumUsers);
 		}
 		else{
 			console.log('Not logged in as an Admin')
 		}
 		this.setState({
         	userList: currUsers,
-			userId: currId
+			userId: currId,
+			numUsers: currNumUsers
       	});
 	}
 	
@@ -61,11 +104,15 @@ class Admin extends React.Component {
 		let rowId = e.target.parentElement.parentElement.id;
 		e.preventDefault();
 		let currUsers = [...this.state.userList];
+		let currNumUsers = this.state.numUsers;
+		let currStatsTable = [...this.state.statsTable];
 	    if(this.state.adminLoggedIn == true){
             for (let i = 0; i < currUsers.length; i++){
 				if (currUsers[i].key === rowId){
-					console.log("Deleting User");
+					console.log('Deleting User');
 					currUsers.splice(i,1);
+					currNumUsers--;
+					currStatsTable = this.updateTable([...this.state.statsTable], 'AdminStatisticTNU', 'Total Number of Users', currNumUsers);
 				}
 			}
 		}
@@ -73,15 +120,90 @@ class Admin extends React.Component {
 			console.log('Not logged in as an Admin')
 		}
 		this.setState({
-        	userList: currUsers
+        	userList: currUsers,
+			numUsers: currNumUsers,
+			statsTable: currStatsTable
+      	});
+	}
+	
+	addEvent = (e) => {
+		e.preventDefault();
+		let currEvents = [...this.state.eventList];
+		let currId = this.state.eventId;
+		let currNumEvents = this.state.numEvents;
+		let currStatsTable = [...this.state.statsTable];
+		const newEventName = document.querySelector('#newEventName').value;
+		let newHostName = document.querySelector('#newHostName').value;
+		console.log(newHostName);
+		if (newHostName === ''){
+			newHostName = 'Admin' + this.id;
+		}
+	    if(this.state.adminLoggedIn == true){
+            console.log('adding event');
+			currEvents.push(<tr id={'Event:' + newEventName + 'EventID:' + currId} key={'Event:' + newEventName + 'EventID:' + currId}>
+							<td class = 'TableContents'>{newEventName}</td>
+							<td class = 'TableContents'>{currId}</td>
+							<td class = 'TableContents'>{newHostName}</td>
+							<td class = 'TableButtonCell'><button class = 'TableButton' onClick = {this.goToEvent}>To Event</button></td>
+							<td class = 'TableButtonCell'><button class = 'TableButton' onClick = {this.goToProfile}>To Profile</button></td>
+							<td class = 'TableButtonCell'><button class = 'TableButton' onClick = {this.removeEvent}>X</button></td></tr>);	
+			currId++;
+			currNumEvents++;
+			currStatsTable = this.updateTable([...this.state.statsTable], 'AdminStatisticTNE', 'Total Number of Events', currNumEvents);
+		}
+		else{
+			console.log('Not logged in as an Admin')
+		}
+		this.setState({
+        	eventList: currEvents,
+			eventId: currId,
+			numEvents: currNumEvents,
+			statsTable: currStatsTable
+      	});
+	}
+	
+	removeEvent = (e) => {
+		e.preventDefault();
+		let rowId = e.target.parentElement.parentElement.id;
+		let currNumEvents = this.state.numEvents;
+		let currEvents = [...this.state.eventList];
+		let currStatsTable = [...this.state.statsTable];
+	    if(this.state.adminLoggedIn == true){
+            for (let i = 0; i < currEvents.length; i++){
+				if (currEvents[i].key === rowId){
+					console.log('Deleting Event');
+					currEvents.splice(i,1);
+					currNumEvents--;
+					currStatsTable = this.updateTable([...this.state.statsTable], 'AdminStatisticTNE', 'Total Number of Events', currNumEvents);
+				}
+			}
+		}
+		else{
+			console.log('Not logged in as an Admin')
+		}
+		this.setState({
+        	eventList: currEvents,
+			numEvents: currNumEvents,
+			statsTable: currStatsTable
       	});
 	}
 
+	updateTable = (statTable, statKey, statName, newValue) =>{
+		for(let i = 0; i < statTable.length; i++){
+			if(statTable[i].key === statKey){
+				statTable.splice(i, 1, <tr id={statKey} key={statKey}>
+									   <td class = 'TableContents'> {statName}</td>
+									   <td class = 'TableContents'>{ newValue }</td></tr>);
+			}
+		}
+		return statTable;
+	}
+	
     render()  {
 		if(this.adminLoggedIn == false){
 			return (
             <div id='AdminDenied'>
-				<h1> PERMISSION DENIED</h1>
+				<h1>PERMISSION DENIED</h1>
 			 </div>
           );
 		}
@@ -89,31 +211,79 @@ class Admin extends React.Component {
 			return (
 				<div id='AdminBody'>
 					<h1>Admin ID: {this.id}</h1>
+					<h4 id='UserListHeader'>User List</h4>
+					<h4 id='AdminEventListHeader'>Event List</h4>
+					<br></br>
 					<form id='UserForm'>
-						<input id='newUser' type="text" placeholder="Full Name"/>
+						<input id='newUser' type='text' placeholder='Full Name'/>
 						<button onClick={ this.addUser }>Add User</button>
 					</form>
-					<div id = 'UserTableDiv'>
-						<table id='UserList'>
-							<tbody>
-								<tr>
-									<th class = 'TableContents'>
-										Name
-									</th>
-									<th class = 'TableContents'>
-										UserID
-									</th>
-									<th class = 'TableButton'>
-										Profile
-									</th>
-									<th class = 'TableButton'>
-										Remove
-									</th>
-								</tr>
-								{ this.state.userList }
-							</tbody>
-						</table>
-					</div>
+					<form id='EventAddForm'>
+						<input id='newEventName' type='text' placeholder='Event Name'/>
+						<input id='newHostName' type='text' placeholder='Host Name'/>
+						<button onClick={ this.addEvent }>Add Event</button>
+					</form>
+					<br></br>
+					<table id='UserList'>
+						<tbody>
+							<tr>
+								<th class = 'TableContents'>
+									Name
+								</th>
+								<th class = 'TableContents'>
+									UserID
+								</th>
+								<th class = 'TableButtonCell'>
+									Profile Link
+								</th>
+								<th class = 'TableButtonCell'>
+									Remove
+								</th>
+							</tr>
+							{ this.state.userList }
+						</tbody>
+					</table>
+					<table id='AdminEventList'>
+						<tbody>
+							<tr>
+								<th class = 'TableContents'>
+									Event Name
+								</th>
+								<th class = 'TableContents'>
+									EventID
+								</th>
+								<th class = 'TableContents'>
+									Host Name
+								</th>
+								<th class = 'TableButtonCell'>
+									Event Link
+								</th>
+								<th class = 'TableButtonCell'>
+									Profile Link
+								</th>
+								<th class = 'TableButtonCell'>
+									Remove
+								</th>
+							</tr>
+							{ this.state.eventList }
+						</tbody>
+					</table>
+					<br></br>
+					<h4 id='OverallStatsHeader'>Overall Stats</h4>
+					<br></br>
+					<table id='OverallStatsTable'>
+						<tbody>
+							<tr>
+								<th class = 'TableContents'>
+									Statistic Name
+								</th>
+								<th class = 'TableContents'>
+									Statistic
+								</th>
+							</tr>
+							{this.state.statsTable}
+						</tbody>
+					</table>
 				</div>
 			);
 		}
