f9e622f06d7cf4f461918650d31e8e42c612073b
Wilson Hsu <wilsonhsu0102@gmail.com>
2019-11-26 16:03:54 -0500

    updated eventSchema

--- uni/src/models/event.js
+++ uni/src/models/event.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 
 let eventSchema = new mongoose.Schema({
+    host: { type: mongoose.Schema.Types.ObjectId, ref: 'Account'},
     eventName: String,
     location: String,
-    date: String
+    description: String,
+    date: Date,
+    attendees: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Account'}]
 });
 
 module.exports = mongoose.model('Event', eventSchema)
