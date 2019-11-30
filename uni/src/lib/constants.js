7cd3480a1117f9ced2d349a93fcda220679fa2e7
fishnyips <crystal.yip@mail.utoronto.ca>
2019-11-30 15:02:57 -0500

    update constants

--- uni/src/lib/constants.js
+++ uni/src/lib/constants.js
@@ -8,7 +8,7 @@ module.exports = {
     MONGO_DB_PORT: '27017',
     MONGO_DB_NAME: 'uni',
     //MONGO_DB_URL: 'mongodb://localhost:27017/uni',
-    MONGO_DB_URL: 'mongodb://<dbuser>:<dbpassword>@ds251158.mlab.com:51158/heroku_m56ts9mx',
+    MONGO_DB_URL: 'mongodb://dbuser:Uni123@ds251158.mlab.com:51158/heroku_m56ts9mx',
     //HEROKU_PORT: ''
     PORT: ':5000',
 }
