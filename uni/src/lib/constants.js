d17c74459963781dd71a6377c88145d3f9cd200d
fishnyips <crystal.yip@mail.utoronto.ca>
2019-11-30 14:24:55 -0500

    testing

--- uni/src/lib/constants.js
+++ uni/src/lib/constants.js
@@ -7,7 +7,8 @@ module.exports = {
     MONGO_DB: 'mongodb://localhost:',
     MONGO_DB_PORT: '27017',
     MONGO_DB_NAME: 'uni',
-    MONGO_DB_URL: 'mongodb://localhost:27017/uni',
+    //MONGO_DB_URL: 'mongodb://localhost:27017/uni',
+    MONGO_DB_URL: 'mongodb+srv://adminUser:123@cluster0-ralx9.mongodb.net/test?retryWrites=true&w=majority',
     //HEROKU_PORT: ''
     PORT: ':5000',
 }
