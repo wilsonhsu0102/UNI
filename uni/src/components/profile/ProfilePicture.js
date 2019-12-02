586564fee22664192da5548e94b653cfb1cc955c
chrison <chris.on@mail.utoronto.ca>
2019-12-02 12:14:57 -0500

    main profile info and profile picture now store on account instead of profile

--- uni/src/components/profile/ProfilePicture.js
+++ uni/src/components/profile/ProfilePicture.js
@@ -1,27 +1,60 @@
 import React from "react";
-import './ProfilePage.css'
+import './ProfilePage.css';
+import constants from '../../lib/constants';
+import axios from 'axios'; 
 
 export default class ProfilePicture extends React.Component {
 	constructor(props) {
-		super(props);
-		
-		this.profileId = Number(this.props.id);
-		// will retrieve name and profile image data from database
-
-		if (this.profileId !== 1) {
-			this.name = 'Wilson Hsu'
-			this.ProfilePicture = require('../../images/profilepic.jpg')
-		} else {
-			this.name = 'Authur Fleck'
-			this.ProfilePicture = require('../../images/coverPhoto1.jpg')
+		super(props)
+		this.state = {
+			account: {},
+			pictures: []
 		}
 	}
 
+	componentDidMount(){
+        console.log("Loading profile picture for profile.")
+		axios.get(`${constants.HTTP}${constants.HOST}${constants.PORT}/images/all`, {withCredentials: true})
+        .then(res => {
+			let filtered = []
+			console.log(res.data)
+			res.data.forEach((pic) => {
+				if (pic.type === 'profilepic') {
+					filtered.push({img: pic.path, title: "", author: "", cols: 1})
+				}
+			})
+			console.log(filtered)
+            this.setState({
+                pictures: filtered
+            })
+        })
+        .catch(err => {
+            console.log(err);
+        })
+        .finally(() => {
+		})
+		axios.get(`${constants.HTTP}${constants.HOST}${constants.PORT}/student/getAccount`, {withCredentials: true})
+		.then(res => {
+			
+            this.setState({
+                account: res.data
+			})
+			console.log(this.state.account)
+        })
+        .catch(err => {
+            console.log(err);
+        })
+        .finally(() => {
+		
+		})
+	}	
+
 	render() {
+		console.log(this.state.account.profilePicture)
 		return (
 			<div>
-				<img className='profilepic' src={this.ProfilePicture} alt='Me'/>
-				<h1 className='profilename'> {this.name} </h1>
+				<img className='profilepic' src={this.state.account.profilePicture} alt='Me'/>
+				<h1 className='profilename'> {this.state.account.name} </h1>
 			</div>
 		);	
 	}
