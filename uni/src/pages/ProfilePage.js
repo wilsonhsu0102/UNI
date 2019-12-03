e265a4022ebd9941407cafcaa5ddc98cbd9152b8
Wilson Hsu <wilsonhsu0102@gmail.com>
2019-12-02 19:26:13 -0500

    lala

--- uni/src/pages/ProfilePage.js
+++ uni/src/pages/ProfilePage.js
@@ -9,61 +9,64 @@ import ProfileEdit from '../components/profile/ProfileEdit'
 import '../components/profile/ProfilePage.css'
 import NavBar from '../components/navbar';
 import PermissionDenied from '../pages/PermissionDenied'
-import { SessionContext, getSessionCookie, setSessionCookie } from "../session";
+import { SessionContext, getSessionCookie, setSessionCookie, removeSessionCookie } from "../session";
 import constants from '../lib/constants'
 import axios from 'axios'; 
+import PacmanLoader from 'react-spinners/PacmanLoader';
+
 
 class Profile extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            account: {}
+            account: {},
+            loading: true
         }
         console.log(this.props.id)
     }
 
-    componentDidMount(){
-        console.log("Loading profile page.")
-        axios.get(`${constants.HTTP}${constants.HOST}${constants.PORT}/student/getAccount/${this.props.id}`, {withCredentials: true})
-		.then(res => {
-			
+    componentDidMount() {
+        setTimeout(() => {
             this.setState({
-                account: res.data
+                loading: false
             })
-            console.log(res)
-			console.log(this.state.account)
-        })
-        .catch(err => {
-            console.log(err);
-        })
-        .finally(() => {
-		
-		})
+        }, 5000)
+    }
+
+    loading = () => {
+        const session = getSessionCookie()
+        if (session) {
+            this.host = session;
+            console.log(session)
+            return [<NavBar id = {this.props.id} key={"NavBar"}></NavBar>, <div className='sweet-loading'>
+                <PacmanLoader
+                color={'rgb(245, 150, 164)'}
+                loading={this.state.loading}
+                />
+            </div>]
+        }
+        removeSessionCookie()
+        return <Login></Login>
     }
 
 
-    // renderCondition() {
-    //     console.log("rendercondition profile page", parseInt(this.state.account._id))
-    //     const session = getSessionCookie()
-    //     if (session !== undefined) {
-    //         return [<NavBar id ={this.state.account._id}></NavBar>,
-    //         <ProfilePicture id={this.state.account._id} key={0}/>, 
-    //         <SelfIntro id={this.state.account._id} key={1}/>,
-    //         <Bio id={this.state.account._id} key={2}/>, 
-    //         <HiddenInfo id={this.state.account._id} key={3}/>, 
-    //         <PhotoLibrary id={this.state.account._id} key={4}/>]
-    //     } else {
-    //         return <Login></Login>
-    //     }
-    // }
-
-    render() {
-        return [<NavBar id ={this.props.id}></NavBar>,
+    renderCondition() {
+        console.log("rendercondition profile page", parseInt(this.state.account._id))
+        const session = getSessionCookie()
+        if (session) {
+            return [<NavBar id ={this.props.id}></NavBar>,
             <ProfilePicture id={this.props.id} key={0}/>, 
-            <SelfIntro id={this.props.id} key={1}/>, 
+            <SelfIntro id={this.props.id} key={1}/>,
             <Bio id={this.props.id} key={2}/>, 
             <HiddenInfo id={this.props.id} key={3}/>, 
             <PhotoLibrary id={this.props.id} key={4}/>]
+        } 
+        removeSessionCookie()
+        return <Login></Login>
+    }
+
+    render() {
+        return (this.state.loading ? this.loading() : this.renderCondition())
     } 
   }
 export default Profile;
