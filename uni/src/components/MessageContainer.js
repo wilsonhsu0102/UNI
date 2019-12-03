1281b8a5cb07d20c3cb7341779d8629f305f5c9f
fishnyips <crystal.yip@mail.utoronto.ca>
2019-12-03 18:12:30 -0500

    fix chat page issue?

--- uni/src/components/MessageContainer.js
+++ uni/src/components/MessageContainer.js
@@ -4,8 +4,6 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import ChatMessage from './ChatMessage'
 import './MessageContainer.css'
-import constants from '../lib/constants';
-const datetime = require('date-and-time');
 
 /* Component for the Message Container */
 class MessageContainer extends React.Component {
@@ -32,88 +30,12 @@ class MessageContainer extends React.Component {
 	componentWillUnmount() {
 		clearInterval(this.interval);
 	}
-
-	messageChangeHandler = event => {
-        this.setState({
-			message: event.target.value
-        });
-	}
-	
-	messageRequest (messObj){
-		return new Promise((resolve, reject) => {
-			fetch(constants.HTTP + constants.HOST + constants.PORT + '/chats/sendMessage', {
-				method: "POST",
-				body: JSON.stringify(messObj),
-				credentials: 'include',
-				headers: {
-				"Access-Control-Allow-Credentials": "true",
-				"Content-type": "application/json; charset=UTF-8"
-			}})
-			.then(res => res.json())
-			.then(
-				(result) => {
-					//console.log('messageObject: ', result);
-					resolve(result);
-				},
-				(error) => {
-					reject('issue with getting resource');
-				}
-			)
-		})
-		
-	}
-	
-	sendHandler = event => {
-		if (this.state.message !== ""){
-			const now = new Date();
-			const messObj = {
-				userId: this.state.userId,
-				message: this.state.message,
-				timestamp: now,
-				combinedId: this.state.combinedId
-			}
-			this.messageRequest(messObj).then((result) => {
-				if (this.state.timestamp === null){
-					const now = datetime.parse(new Date(), 'YYYY/MM/DD HH:mm:ss');
-					this.setState({
-						messages: result.messages,
-						timestamp: now,
-						message: ""
-					});
-				}
-				else if((result.timestamp !== null)){
-					this.setState({
-						messages: result.messages,
-						timestamp: result.timestamp,
-						message: ""
-					});
-				}
-				document.querySelector("#userMessageInput").value = "";
-			}).catch((error) => { console.log(error)});
-		}
-	}
 	
     render() {
 		const { userId, userName, connectionData, messages} = this.props.params
-		if(!connectionData){
-			return (<div id="messageDiv">An error has occurred</div>);
-		} 
-
-		if (!this.props.params.messages) {
-			return (<div id="messageDiv">
-				<h4 id="connectName">
-					<img id="connectAvatar" src={`data:image/png;base64,${connectionData.profilePicture}`}/>
-					{connectionData.name} 
-					<p id="connectEmail"> {connectionData.email}</p>
-				</h4>
-				<div id="chatContainer" onClick = {this.scrollHandler}>
-					
-				</div>
-				<form id="messageForm">
-					<input id="userMessageInput" type="text" placeholder="Type a message" onChange={this.messageChangeHandler}></input>
-					<Button id="sendButton" variant="outlined" onClick={this.sendHandler}>SEND</Button>
-				</form>
-			</div>)
+		const {sendHandler, messageHandler} = this.props
+		if(!connectionData || !this.props.params.messages){
+			return (<div id="messageDiv">Loading Page. Please Wait.</div>);
 		}
         return (
             <div id="messageDiv">
@@ -138,8 +60,8 @@ class MessageContainer extends React.Component {
 					}
 				</div>
 				<form id="messageForm">
-					<input id="userMessageInput" type="text" placeholder="Type a message" onChange={this.messageHandler}></input>
-					<Button id="sendButton" variant="outlined" onClick={this.sendHandler}>SEND</Button>
+					<input id="userMessageInput" type="text" placeholder="Type a message" onChange={messageHandler}></input>
+					<Button id="sendButton" variant="outlined" onClick={sendHandler}>SEND</Button>
 				</form>
             </div>
         );
